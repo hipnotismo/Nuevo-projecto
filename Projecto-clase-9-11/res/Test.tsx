168efad9caeb8ab4d06bc81bb1f14d2d7f6110ce
unknown <pehuen-testa@hotmail.com>
2018-11-09 11:47:56 -0300

    SFML integrado-falta realese

---
+++ Projecto-clase-9-11/res/Test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Test" tilewidth="32" tileheight="32" tilecount="228" columns="12">
+ <image source="TileKit.png" width="384" height="608"/>
+</tileset>
