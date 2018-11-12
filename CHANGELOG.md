d6131329164e29ccf83b0f80ad91a16df3c3d88b
unknown <martinconcetti97@gmail.com>
2018-11-12 06:18:56 -0300

    Personaje agregado

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,9 @@
 Changelog
 
-[0.2] - 9-11-2018
+[0.2] - 12-11-2018
 -Tile map del fondo agregado
+-Personaje agregado
+-Gravedad agregada
 
 [0.1] - 9-11-2018
 -Librerias integradas
--- Projecto-clase-9-11/src/game.cpp
+++ Projecto-clase-9-11/src/game.cpp
@@ -4,28 +4,53 @@
 
 #include "game.h"
 
-
+struct Plat1
+{
+	int x = 200;
+	int y = 155;
+};
 namespace Game
 {
 	void core()
 	{
 		sf::RenderWindow window(sf::VideoMode(700, 300), "STP Example");
+		window.setFramerateLimit(60);
+
 		tmx::TileMap map("res/Mapa.tmx");
 
-		sf::Texture t1, t2, t3;
+		sf::Texture t1;
+
+		t1.loadFromFile("res/Character.png");
 		
+		sf::Sprite Char(t1);
+
+		int x = 100, y = 100, h = 200;
+		float dx = 0, dy = 0;
+
 		while (window.isOpen()) {
 			// Process events
 			sf::Event event;
-			while (window.pollEvent(event)) {
+			while (window.pollEvent(event)) 
+			{
 				// Close window : exit
 				if (event.type == sf::Event::Closed)
 					window.close();
 			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) x += 3;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) x -= 3;
+
+			dy += 0.2;
+			y += dy;
+			if (y>155)  dy = 0;
+
+			Char.setPosition(x, y);
+
 			// Clear screen
 			window.clear();
 			// Draw the map
 			window.draw(map);
+			window.draw(Char);
 			// Update the window
 			window.display();
 		}
