1fdc9b4b98780e23bfa9e9ef194788807dc36d5d
unknown <martinconcetti97@gmail.com>
2018-11-12 21:13:06 -0300

    Mecanicas basicas

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 Changelog
 
+[0.3] - 12-11-2018
+-Se agregaron limites a la pantalla para que el jugador no se salga
+-El jugador vuelva a la posicion original si toca el agua
+-Movimiento basico y saltos
+
 [0.2] - 12-11-2018
 -Tile map del fondo agregado
 -Personaje agregado
--- Projecto-clase-9-11/src/game.cpp
+++ Projecto-clase-9-11/src/game.cpp
@@ -11,21 +11,25 @@ struct Plat1
 };
 namespace Game
 {
+	
+
 	void core()
 	{
 		sf::RenderWindow window(sf::VideoMode(700, 300), "STP Example");
 		window.setFramerateLimit(60);
 
 		tmx::TileMap map("res/Mapa.tmx");
-
+		
 		sf::Texture t1;
 
 		t1.loadFromFile("res/Character.png");
 		
 		sf::Sprite Char(t1);
 
-		int x = 100, y = 100, h = 200;
-		float dx = 0, dy = 0;
+		static int Init_Char_x = 100;
+		static int Init_Char_y = 100;
+		static int x = Init_Char_x, y = Init_Char_y, h = 200;
+		static float dx = 0, dy = 0;
 
 		while (window.isOpen()) {
 			// Process events
@@ -39,10 +43,45 @@ namespace Game
 
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) x += 3;
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) x -= 3;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) y -= 7;
 
 			dy += 0.2;
 			y += dy;
-			if (y>155)  dy = 0;
+
+			if (x <= 285) 
+			{
+				if (y > 155)
+				{
+					dy = 0;
+				}
+			}
+
+			if (x >= 285 && x <= 330)
+			{
+				if (y > 100)
+				{
+					x = Init_Char_x;
+					y = Init_Char_y;
+				}
+			}
+
+			if (x >= 330)
+			{
+				if (y > 155)
+				{
+					dy = 0;
+				}
+			}
+
+			if (x >= 650)
+			{
+				x -= 3;
+			}
+
+			if (x <= 0)
+			{
+				x += 3;
+			}
 
 			Char.setPosition(x, y);
 
@@ -51,6 +90,7 @@ namespace Game
 			// Draw the map
 			window.draw(map);
 			window.draw(Char);
+
 			// Update the window
 			window.display();
 		}
